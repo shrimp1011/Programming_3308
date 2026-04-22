2c09ee7e788330a32f9b5cc96a0e899feda82ac2
Yun seowoo <seowoo06236@gmail.com>
2026-04-22 09:32:35 +0900

    TodoListApp <TodoItem key={}>

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -8,7 +8,7 @@ export default function TodoList() {
             {todos.length === 0 && <TodoItemEmpty />}
             {/* todos에 값이 있으면, TodoItem에 todo를 넣자 */}
             {todos.length > 0&&
-                todos.map((todo) =><TodoItem todo={ todo } />)}
+                todos.map((todo) =><TodoItemm key={todo.id} todo={ todo } />)}
         </ul>
     )
 }
